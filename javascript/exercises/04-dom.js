bf759f781ab3e85684e18b9acc6f5129b2c12348
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2023-10-29 11:07:47 -0700

    finished dom exersise

--- javascript/exercises/04-dom.js
+++ javascript/exercises/04-dom.js
@@ -3,15 +3,33 @@
 
 // fetch JavaScript objects representing specific elements in the DOM
 
+let userInput = document.querySelector("#userInput1");
+let copy = document.querySelector("#copy");
+let output = document.querySelector("#output");
+
 // add an event listener on the target element
+copy.addEventListener("click", handleCLick);
 
 // callback function to handle event
+function handleCLick(event) {
+  output.textContent = userInput.value;
+}
 
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let userInput2 = document.querySelector("#userInput2");
 
 // add an event listener on the target element
+userInput2.addEventListener("input", handleInput);
+
+let section = document.querySelector("#inputEventExample");
+let element = document.createElement("div");
+element.setAttribute("class", "output");
+section.append(element);
 
 // callback function to handle event
+function handleInput(event) {
+  element.textContent = userInput2.value;
+}
