5d2ff84e4d47067d9cde5e3b9eb836207e3b92d1
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2023-11-24 19:44:22 -0800

    completed dom jquery exercise

--- jquery/exercises/01-dom.js
+++ jquery/exercises/01-dom.js
@@ -3,7 +3,7 @@
 
 // Option #1
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput1 = document.querySelector('#userInput1');
+/*let userInput1 = document.querySelector('#userInput1');
 let copy = document.querySelector('#copy');
 let output1 = document.querySelector('#output1');
 
@@ -15,6 +15,13 @@ function handleClick(event) {
   console.log('click event', event);
   output1.textContent = userInput1.value;
 }
+*/
+
+$("document").ready(() => {
+  $("#copy").on("click", () => {
+    $("#output1").text($("#userInput1").val());
+  });
+});
 
 // Option #2
 // document.getElementById('copy').onclick = () => {
@@ -33,7 +40,7 @@ function handleClick(event) {
 
 // Exercise #2:
 // When the user enters input text, copy the user input to the output area
-
+/*
 // Fetch JavaScript objects representing specific elements in the DOM
 let userInput2 = document.querySelector('#userInput2');
 let output2 = document.querySelector('#output2');
@@ -46,3 +53,10 @@ function handleInput(event) {
   console.log('click event', event);
   output2.textContent = userInput2.value;
 }
+*/
+
+$("document").ready(() => {
+  $("#userInput2").on("input", () => {
+    $("#output2").text($("#userInput2").val());
+  });
+});
